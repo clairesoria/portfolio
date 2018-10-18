28ac2ac1aa09ed7318db2c2f0c4dd5482667331d
Claire SG <soriagondek@gmail.cm>
2018-10-18 19:06:06 +0200

    portfolio

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+/*====================================================
+
+
+	Table of Contents
+
+	01. Lightbox
+
+
+
+====================================================*/
+
+
+
+jQuery(document).ready(function($) {
+	"use strict";
+
+
+/*======================
+
+	01. Lightbox
+
+========================*/
+
+
+
+
+
+	lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    });
+
+
+
+
+
+});
